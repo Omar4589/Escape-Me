a72460569a35125c2a519888ad790f0b53e0740f
Omar4589 <omar4589@hotmail.com>
2023-04-28 21:52:04 -0500

    completes login GET route handler and login view

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,34 +1,38 @@
 const router = require("express").Router();
 const { User, Booking, EscapeRoom, Leaderboard, Review } = require("../models");
-const withAuth = require("../utils/auth");
+//const withAuth = require("../utils/auth");
 
-router.get("/", async (req, res) => {
-  res.render("welcomePage");
+// router.get("/", async (req, res) => {
+//   res.render("welcomePage");
+// });
+
+router.get("/login", async (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/user/home");
+    return;
+  }
+
+  res.render("loginPage");
 });
 
-router.get('/login', async (req, res) => {
-    if (req.session.logged_in) {
-      res.redirect('/');
-      return;
-    }
-  
-    res.render('login');
-  });
+// router.get("/signup", (req, res) => {
+//   res.render("signupPage");
+// });
 
+// router.get("/leaderboard", (req, res) => {
+//   res.render("leaderboardPage");
+// });
 
+// router.post("/leaderboard", (req, res) => {
+//   res.render("");
+// });
 
-  router.get("/signup", (req, res) => {
-    res.render("signup");
-  });
+// router.get("/reviews", (req, res) => {
+//   res.render("reviewsPage");
+// });
 
-
-
-  router.get("/leaderboard", (req, res) => {
-    res.render("leaderboard");
-  });
-
-  router.post("/leaderboard", (req, res) => {
-    res.render("leaderboard");
-  });
+// router.post("/reviews", (req, res) => {
+//   res.render("");
+// });
 
 module.exports = router;
