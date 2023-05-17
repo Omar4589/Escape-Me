f36fa32248e735552c7006bd53baa5bbd4bc0bd8
Omar4589 <omar4589@hotmail.com>
2023-05-17 00:21:56 -0500

    creates AdminHome view and controller and updates User model to include 'isAdmin'

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const publicRoutes = require('./publicRoutes');
 const privateRoutes = require('./privateRoutes');
+const adminRoutes = require('./adminRoutes');
 
 router.use('/', publicRoutes);
 router.use('/', privateRoutes);
 router.use('/api', apiRoutes);
+router.use('/admin', adminRoutes);
 
 module.exports = router;
--- utils/auth.js
+++ utils/auth.js
@@ -1,9 +1,12 @@
 const withAuth = (req, res, next) => {
-  console.log('Session data:', req.session);
   if (!req.session.logged_in) {
     res.redirect('/login');
   } else {
-    next();
+    if (req.session.isAdmin) {
+      res.redirect('/admin/home');
+    } else {
+      next();
+    }
   }
 };
 
