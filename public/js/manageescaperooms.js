14bad9a8720e9b5b01779bf2c7ffc9f79a44eb7c
Omar4589 <omar4589@hotmail.com>
2023-06-04 22:35:24 -0500

    creates js file for ManageEscapeRooms page and adds hidden modal to page

---
+++ public/js/manageescaperooms.js
@@ -0,0 +1,8 @@
+const roomDivs = $('.escape-room');
+
+roomDivs.on('click', (e) => {
+  const clickedRoom = e.target;
+  console.log(clickedRoom);
+
+  //make api call to fetch for escape room's info 
+});
